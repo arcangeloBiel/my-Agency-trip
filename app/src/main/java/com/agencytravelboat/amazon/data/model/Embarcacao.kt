5f4b9c25d813b0bbda3c3294e84d374021a402b3
joao <joao.ribeiro@havan.com.br>
2023-02-05 08:40:52 -0300

    projeto base

---
+++ app/src/main/java/com/agencytravelboat/amazon/data/model/Embarcacao.kt
@@ -0,0 +1,13 @@
+package com.agencytravelboat.amazon.data.model
+
+import java.io.Serializable
+
+class Embarcacao
+    (
+    var Id: Int? = 0,
+    var Nome: String? = null,
+    var Descricao: String? = null,
+    var Cidade: String? = null,
+    var ImagemUrl: String? = null
+
+): Serializable
---
+++ app/src/main/java/com/agencytravelboat/amazon/presentation/ui/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.agencytravelboat.amazon.presentation.ui
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
